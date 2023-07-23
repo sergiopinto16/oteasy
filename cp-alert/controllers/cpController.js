002dd0a437355452d3b60466f7f290bbd8483bea
reckonaisergiopinto <sergio.pinto@reckon.ai>
2023-07-23 11:54:36 +0100

    new approch to cp-alert

--- cp-alert/controllers/cpController.js
+++ cp-alert/controllers/cpController.js
@@ -10,6 +10,9 @@ station = "9408227"
 url_cp_cete = "https://www.infraestruturasdeportugal.pt/negocios-e-servicos/partidas-chegadas/9408227/2023-07-17%2000:00/2023-07-17%2023:59/INTERNACIONAL,%20ALFA,%20IC,%20IR,%20REGIONAL,%20URB%7CSUBUR,%20ESPECIAL"
 let last_json = null
 let current_json = null
+let today_day = null
+let today_trains = null
+let today_trains_last_update = null
 
 
 // check supppress train
@@ -19,7 +22,24 @@ const checkSuppressTrain = (req, res) => {
     console.log("Check suppress train")
 
     date_string = new Date().toISOString().split('T')[0];
-
+    
+    const d = new Date();
+    const localTime = d.getTime();
+    const localOffset = d.getTimezoneOffset() * 60000;
+    
+    const utc = localTime + localOffset;
+    const offset = 1; // UTC of Dubai is +01.00
+    const portugal = utc + (3600000 * offset);
+    
+    
+    const portugalTimeNow = new Date(portugal).toISOString().split('T')[0];
+    date_string = portugalTimeNow
+    
+    if(date_string !== today_day){
+        console.log("Update data")
+        today_trains = {}
+        today_day = date_string
+    }
     
     url = "https://www.infraestruturasdeportugal.pt/negocios-e-servicos/partidas-chegadas/"+
                 station +"/"+date_string+"%2000:00/"+date_string+
@@ -29,36 +49,48 @@ const checkSuppressTrain = (req, res) => {
         .then(res => res.json())
         .then(text => {
             current_json = text;
+
             
-            if(last_json==null){
+
+            //filter data to add in today_trains by date
+            for(var attributename in current_json['response'][0]['NodesComboioTabelsPartidasChegadas']){
+                json_aux = current_json['response'][0]['NodesComboioTabelsPartidasChegadas'][attributename]
+                // add data to today_trains
+                today_trains[json_aux["DataHoraPartidaChegada_ToOrderBy"]] = json_aux
+            }
+
+            if(today_trains_last_update==null){
                 console.log("Last json is null")        
             }
             else{
-                let dif_json = diff(last_json,current_json)
-                console.log(JSON.stringify(dif_json))
-                
-                for(var attributename in dif_json['response'][0]['NodesComboioTabelsPartidasChegadas']){
-                    console.log(attributename+": "+ JSON.stringify(dif_json['response'][0]['NodesComboioTabelsPartidasChegadas'][attributename]));
-                    if (dif_json['response'][0]['NodesComboioTabelsPartidasChegadas'][attributename]['Observacoes']!=null){
-                            if(dif_json['response'][0]['NodesComboioTabelsPartidasChegadas'][attributename]['Observacoes'].length >0){
-                        console.log("Comboio suprimido!")
-                        console.log(current_json['response'][0]['NodesComboioTabelsPartidasChegadas'][attributename])
-                        data = {
-                            "DataHoraPartidaChegada_ToOrderBy":current_json['response'][0]['NodesComboioTabelsPartidasChegadas'][attributename]['DataHoraPartidaChegada_ToOrderBy'],
-                            "NomeEstacaoOrigem":current_json['response'][0]['NodesComboioTabelsPartidasChegadas'][attributename]['NomeEstacaoOrigem'],
-                            "NomeEstacaoDestino":current_json['response'][0]['NodesComboioTabelsPartidasChegadas'][attributename]['NomeEstacaoDestino'],
-                            "ComboioPassou":current_json['response'][0]['NodesComboioTabelsPartidasChegadas'][attributename]['ComboioPassou'],
-                            "Observacoes":current_json['response'][0]['NodesComboioTabelsPartidasChegadas'][attributename]['Observacoes'],
+                // let dif_json = diff(today_trains_last_update,today_trains)
+                for(var attributename in today_trains){
+                    console.log("attributename = " + attributename)
+                    if (today_trains[attributename]['Observacoes']!=null){
+                        if(today_trains[attributename]['Observacoes'].length >0){
+                            if(today_trains_last_update[attributename]['Observacoes']!=today_trains[attributename]['Observacoes']){
+                                console.log("Comboio suprimido!")
+                                console.log(today_trains[attributename])
+                                data = {}
+                                data = {
+                                    "DataHoraPartidaChegada_ToOrderBy":today_trains[attributename]['DataHoraPartidaChegada_ToOrderBy'],
+                                    "NomeEstacaoOrigem":today_trains[attributename]['NomeEstacaoOrigem'],
+                                    "NomeEstacaoDestino":today_trains[attributename]['NomeEstacaoDestino'],
+                                    "ComboioPassou":today_trains[attributename]['ComboioPassou'],
+                                    "Observacoes":today_trains[attributename]['Observacoes'],
+                                }
+                                console.log(data)
+                                sendSlackNotification(JSON.stringify(data))
                         }
-                        console.log(data)
-                        sendSlackNotification(JSON.stringify(data))
                     }
-                }
+                    }
+                    
+
                 }
                 
             }
-            
-            last_json = current_json;
+        
+            today_trains_last_update = today_trains
         
         });
 
