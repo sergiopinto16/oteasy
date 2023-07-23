2f865b56400ccc55000a1201307e19737ef7741f
reckonaisergiopinto <sergio.pinto@reckon.ai>
2023-07-24 00:27:28 +0100

    new approch to cp-alert, fix error in copy array (use deep copy)

--- cp-alert/controllers/cpController.js
+++ cp-alert/controllers/cpController.js
@@ -14,7 +14,6 @@ let today_day = null
 let today_trains = null
 let today_trains_last_update = null
 
-
 // check supppress train
 // app.post('/check_suppress_train', 
 const checkSuppressTrain = (req, res) => {
@@ -28,12 +27,13 @@ const checkSuppressTrain = (req, res) => {
     const localOffset = d.getTimezoneOffset() * 60000;
     
     const utc = localTime + localOffset;
-    const offset = 1; // UTC of Dubai is +01.00
+    const offset = 1; // UTC of portugal is +01.00
     const portugal = utc + (3600000 * offset);
     
     
     const portugalTimeNow = new Date(portugal).toISOString().split('T')[0];
     date_string = portugalTimeNow
+    console.log("Portugal Time = " + portugalTimeNow)
     
     if(date_string !== today_day){
         console.log("Update data")
@@ -49,14 +49,19 @@ const checkSuppressTrain = (req, res) => {
         .then(res => res.json())
         .then(text => {
             current_json = text;
-
             
+            // help to test
+            // current_json = json_2
+            // if (today_trains_last_update==null){
+            //     console.log("current_json = json_1")
+            //     current_json = json_1
+            // }
 
             //filter data to add in today_trains by date
             for(var attributename in current_json['response'][0]['NodesComboioTabelsPartidasChegadas']){
                 json_aux = current_json['response'][0]['NodesComboioTabelsPartidasChegadas'][attributename]
                 // add data to today_trains
-                today_trains[json_aux["DataHoraPartidaChegada_ToOrderBy"]] = json_aux
+                today_trains[json_aux["DataHoraPartidaChegada_ToOrderBy"]]=json_aux
             }
 
             if(today_trains_last_update==null){
@@ -68,7 +73,8 @@ const checkSuppressTrain = (req, res) => {
                     console.log("attributename = " + attributename)
                     if (today_trains[attributename]['Observacoes']!=null){
                         if(today_trains[attributename]['Observacoes'].length >0){
-                            if(today_trains_last_update[attributename]['Observacoes']!=today_trains[attributename]['Observacoes']){
+                            console.log(today_trains[attributename]['Observacoes'] + " | " + today_trains_last_update[attributename]['Observacoes'])
+                            if(today_trains[attributename]['Observacoes']!==today_trains_last_update[attributename]['Observacoes']){
                                 console.log("Comboio suprimido!")
                                 console.log(today_trains[attributename])
                                 data = {}
@@ -90,7 +96,7 @@ const checkSuppressTrain = (req, res) => {
                 
             }
         
-            today_trains_last_update = today_trains
+            today_trains_last_update =JSON.parse(JSON.stringify(today_trains));
         
         });
 
